3b6e85f9122a1e3b04009fd6eaf7d06c3814b44d
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-09-24 11:29:19 +0300

    created ShowRooms controller

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/test',function()  {return "Goodbye";} );
-Route::get('/rooms', function()  {return "Rooms";} );
+// Route::get('/rooms', 'ShowRoomsController' );
+Route::get('/rooms', '\App\Http\Controllers\ShowRoomsController');
